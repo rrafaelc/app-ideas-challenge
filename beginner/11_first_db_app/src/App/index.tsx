85ed8c5efdbc030950d82d12a8c45577b442b096
Rafael Costa <rafaelcosta8020@gmail.com>
2021-10-19 13:33:50 -0300

    Initial App component

---
+++ beginner/11_first_db_app/src/App/index.tsx
@@ -0,0 +1,17 @@
+import { Container, Header, Main, Footer } from './style';
+
+const App = () => {
+	if (!indexedDB) {
+		//
+	}
+
+	return (
+		<Container>
+			<Header>indexedDB App</Header>
+			<Main>App</Main>
+			<Footer />
+		</Container>
+	);
+};
+
+export default App;
---
+++ beginner/11_first_db_app/src/App/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	max-width: 1280px;
+	margin: 0 auto;
+`;
+
+export const Header = styled.header``;
+
+export const Main = styled.main``;
+
+export const Footer = styled.main``;
