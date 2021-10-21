076ec40c4b9045b7c9b7ed8b3f6781e400d5cd7b
Rafael Costa <rafaelcosta8020@gmail.com>
2021-10-21 08:38:04 -0300

    Footer component done

--- beginner/11_first_db_app/src/components/Footer/index.tsx
+++ beginner/11_first_db_app/src/components/Footer/index.tsx
@@ -1,3 +1,58 @@
+import { useEffect, useState } from 'react';
 import { Footer } from './style';
 
-export default () => <Footer>Footer</Footer>;
+export default () => {
+	// By default not show the consent footer
+	const [showConsent, setShowConsent] = useState(false);
+
+	const handleButtonClick = () => {
+		if (showConsent) {
+			setShowConsent(false);
+			const storage = localStorage.getItem('@rrafaelc:indexeddb');
+
+			if (storage) {
+				const parse = JSON.parse(storage);
+
+				const consent = {
+					...parse,
+					consent: true,
+				};
+
+				localStorage.setItem('@rrafaelc:indexeddb', JSON.stringify(consent));
+			} else {
+				localStorage.setItem(
+					'@rrafaelc:indexeddb',
+					JSON.stringify({ consent: true }),
+				);
+			}
+		}
+	};
+
+	useEffect(() => {
+		const storage = localStorage.getItem('@rrafaelc:indexeddb');
+
+		if (storage) {
+			const { consent } = JSON.parse(storage);
+
+			// If consent undefined then show consent footer
+			if (!consent) {
+				setShowConsent(true);
+			}
+		} else {
+			// If storage is undefined then show consent footer
+			setShowConsent(true);
+		}
+	}, []);
+
+	return (
+		<Footer className={showConsent ? 'show' : ''}>
+			<p>
+				No data is collected or sent to a server, all data is client-side only,
+				data used is for educational purposes.
+			</p>
+			<button type='button' onClick={handleButtonClick}>
+				OK
+			</button>
+		</Footer>
+	);
+};
--- beginner/11_first_db_app/src/components/Footer/style.ts
+++ beginner/11_first_db_app/src/components/Footer/style.ts
@@ -1,8 +1,40 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Footer = styled.footer`
 	margin: 20px 0;
 	width: 937px;
 	height: 40px;
 	background-color: #323232;
+
+	font-size: 18px;
+	border-radius: 10px;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 17px;
+
+	display: none;
+
+	&.show {
+		display: flex;
+	}
+
+	button {
+		padding: 3px 20px;
+		border-radius: 9px;
+
+		font-size: 14px;
+		font-weight: bold;
+
+		background: #fee440;
+		color: #000;
+
+		transition: background-color 0.2s;
+	}
+
+	button:hover {
+		background-color: ${shade(0.4, '#fee440')};
+	}
 `;
