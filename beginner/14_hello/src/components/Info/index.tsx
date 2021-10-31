63cecf04123cae85ec4fe5a1f8da88927eb77e1f
Rafael Costa <rafaelcosta8020@gmail.com>
2021-10-31 19:32:16 -0300

    Get user info done, challenge done

--- beginner/14_hello/src/components/Info/index.tsx
+++ beginner/14_hello/src/components/Info/index.tsx
@@ -3,11 +3,25 @@ import './styles.css';
 
 import { useUserInfo, useUserLogin } from '../../context/UserInfo';
 
+type Region = {
+	ip: string;
+	city: string;
+	region: string;
+	country_name: string;
+	postal: string; // zip
+	longitude: number;
+	latitude: number;
+	timezone: string;
+};
+
 export const Info = () => {
 	const [loading, setLoading] = useState(true);
 	const [hello, setHello] = useState('');
-
 	const { codeLanguage, username } = useUserInfo();
+	const [region, setRegion] = useState<Region>({} as Region);
+
+	const [helloError, setHelloError] = useState(false);
+
 	const { setIsLoggedIn } = useUserLogin();
 
 	// https://stackoverflow.com/questions/42361689/implement-html-entity-decode-in-react-js
@@ -18,21 +32,36 @@ export const Info = () => {
 		});
 
 	useEffect(() => {
-		fetch(
-			codeLanguage
-				? `https://fourtonfish.com/hellosalut/?lang=${codeLanguage}`
-				: 'https://fourtonfish.com/hellosalut/?mode=auto',
-		)
-			.then(response => response.json())
-			.then(data => {
-				setHello(data.hello);
-				setLoading(false);
-			})
-			.catch(err => {
-				console.error(err);
-				alert('An error occurred while processing your request');
-			});
-	}, []);
+		const fetchData = async () => {
+			await Promise.all([
+				fetch(
+					codeLanguage
+						? `https://fourtonfish.com/hellosalut/?lang=${codeLanguage}`
+						: 'https://fourtonfish.com/hellosalut/?mode=auto',
+				)
+					.then(response => response.json())
+					.then(data => {
+						setHello(data.hello);
+					})
+					.catch(() => {
+						setHelloError(true);
+						alert('An error occurred while processing your request.');
+					}),
+				fetch(`https://ipapi.co/json/`)
+					.then(response => response.json())
+					.then(data => {
+						setRegion(data);
+					})
+					.catch(() => {
+						alert('An error occurred while processing your request.');
+					}),
+			]);
+
+			setLoading(false);
+		};
+
+		fetchData();
+	}, [codeLanguage]);
 
 	return loading ? (
 		<div className='loading'>
@@ -41,23 +70,36 @@ export const Info = () => {
 		</div>
 	) : (
 		<div className='infoContainer'>
-			<p>
-				{renderHTML(hello)} {username}
-			</p>
+			{helloError ? (
+				<p>
+					<span className='hello'>Error</span> {username}
+				</p>
+			) : (
+				<p>
+					{renderHTML(hello)} {username}
+				</p>
+			)}
 			<p>you have successfully logged in!</p>
 
 			<div className='info'>
 				<p>
-					Info provided by <a href='/'>IP-API</a>
+					Info provided by{' '}
+					<a
+						href='http://ip-api.com/json/'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						IP-API
+					</a>
 				</p>
-				<p>IP address: 192.168.0.1</p>
-				<p>City: Sao Paulo</p>
-				<p>Region: SP</p>
-				<p>Country: Brazil</p>
-				<p>Zip Code: 11111</p>
-				<p>Longitude: -58.1687</p>
-				<p>Latitude: -12.2061</p>
-				<p>Timezone: America/Sao_Paulo</p>
+				<p>IP address: {region.ip || 'Error'}</p>
+				<p>City: {region.city || 'Error'}</p>
+				<p>Region: {region.region || 'Error'}</p>
+				<p>Country: {region.country_name || 'Error'}</p>
+				<p>Zip Code: {region.postal || 'Error'}</p>
+				<p>Longitude: {region.longitude || 'Error'}</p>
+				<p>Latitude: {region.latitude || 'Error'}</p>
+				<p>Timezone: {region.timezone || 'Error'}</p>
 			</div>
 
 			<button type='button' onClick={() => setIsLoggedIn(false)}>
